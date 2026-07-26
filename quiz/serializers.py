a7e7effc666cc733e63156145f59512565321cda
Mohlaroy <mganijonova861@gmail.com>
2026-07-26 23:30:47 +0500

    nextt

--- quiz/serializers.py
+++ quiz/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
 
-from users.serializers import StudentCreateSerializer
+from users.serializers import UserSerializer
 from .models import *
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'name', 'students')
+        fields = '__all__'
 
 
 class GroupSafeSerializer(serializers.ModelSerializer):
-    students = StudentCreateSerializer(many=True, read_only=True)
+    students = UserSerializer(many=True, read_only=True)
+    teacher = UserSerializer(read_only=True)
 
     class Meta:
         model = Group
-        fields = ('id', 'name', 'students')
+        fields = '__all__'
